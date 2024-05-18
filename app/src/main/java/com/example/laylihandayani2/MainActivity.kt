55dda3c4f16a124a1ba8cc9d11d6c9a8e35da0b8
farisjayadi223 <farisjayadi223@gmail.com>
2024-05-18 21:58:30 +0700

    utslayli

---
+++ app/src/main/java/com/example/laylihandayani2/MainActivity.kt
@@ -0,0 +1,10 @@
+package com.lembain.ts
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bund
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
